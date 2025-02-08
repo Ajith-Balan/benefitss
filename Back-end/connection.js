fe06cfea4aec6df1a4e896930d8af95b92856767
AJITH B <139620991+Ajith-Balan@users.noreply.github.com>
2025-02-08 10:35:04 +0530

    Update connection.js

--- Back-end/connection.js
+++ Back-end/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export default async function connection(){
     console.log(process.env.DB_NAME);
-   const URL=process.env.DB_URL+process.env.DB_NAME
+   const URL=process.env.dburl
    console.log(URL);
 
    const db=await mongoose.connect(URL)
